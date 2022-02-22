7391f218542157e8f94de1652d9c3de918d849ec
Bruno Barbosa <brb.dev@outlook.com>
2022-02-22 14:58:54 -0400

    move arquivos um nivel acima (back)

---
+++ back/src/main/java/com/pi/api/entity/Category.java
@@ -0,0 +1,68 @@
+package com.pi.api.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String location;
+    private String description;
+    private String image;
+
+    public Category() {
+    }
+
+    public Category(String title, String location, String description, String image) {
+        this.title = title;
+        this.location = location;
+        this.description = description;
+        this.image = image;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
