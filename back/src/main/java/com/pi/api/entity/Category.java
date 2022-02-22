b53878691f02e182dc869b78a9735309fae43854
Bruno Barbosa <brb.dev@outlook.com>
2022-02-22 15:12:23 -0400

    remove o atributo location do model Category

--- back/src/main/java/com/pi/api/entity/Category.java
+++ back/src/main/java/com/pi/api/entity/Category.java
@@ -12,16 +12,14 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String title;
-    private String location;
     private String description;
     private String image;
 
     public Category() {
     }
 
-    public Category(String title, String location, String description, String image) {
+    public Category(String title, String description, String image) {
         this.title = title;
-        this.location = location;
         this.description = description;
         this.image = image;
     }
@@ -40,14 +38,6 @@ public class Category {
 
     public void setTitle(String title) {
         this.title = title;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
     }
 
     public String getDescription() {
