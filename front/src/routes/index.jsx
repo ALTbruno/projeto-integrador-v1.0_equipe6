139c09000e0aa288d8bd6ba68882c8319807dbda
david115x0 <davidsilvafs29@gmail.com>
2022-02-23 11:17:49 -0300

    Header e footer

--- front/src/routes/index.jsx
+++ front/src/routes/index.jsx
@@ -1,13 +1,16 @@
 import Home from '../pages/Home';
+import Footer from '../components/Footer';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Header from '../components/header';
 
-
-const  RouteList =() => (
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                </Routes>
-            </BrowserRouter>
+const RouteList = () => (
+    <BrowserRouter>
+        <Header />
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+    </BrowserRouter>
 )
 
 export default RouteList;
