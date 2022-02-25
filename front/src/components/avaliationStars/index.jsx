1c9c6d0dc05c19565da602fd13a2f26751107b6e
William Lima <wilima010@gmail.com>
2022-02-25 10:16:43 -0300

    Cards atualizados

---
+++ front/src/components/avaliationStars/index.jsx
@@ -0,0 +1,20 @@
+import { Button } from 'react-bootstrap';
+import { RiStarSLine, RiStarSFill } from 'react-icons/ri';
+
+function Rating() {
+
+    function avaliacao () {
+
+    }
+  return (
+    <>
+        <RiStarSLine className='text-primary'/>
+        <RiStarSLine className='text-primary'/>
+        <RiStarSLine className='text-primary'/>
+        <RiStarSLine className='text-primary'/>
+        <RiStarSLine className='text-primary'/>
+    </>
+  );
+}
+
+export default Rating;
--- front/src/components/recomendationList/index.jsx
+++ front/src/components/recomendationList/index.jsx
@@ -1,5 +1,7 @@
 import { Button, Card, CardGroup, Container, Row, Col, Stack } from "react-bootstrap";
-import hoteis from "./hoteis.json"
+import Rating from "../avaliationStars";
+import hoteis from "./hoteis.json";
+import { MdRoom } from "react-icons/md";
 
 function RecomendationList () {
 
@@ -9,23 +11,23 @@ function RecomendationList () {
 
     return (
         <>
-        
             <Row className="justify-content-center">
                 <h3 className="my-3 ms-5">Recomendações</h3>
                 {hoteis.map(({ foto, nome, localizacao, descricao }) => {
 
                     return (  
-                        <Card className="mx-3 my-2 d-flex flex-row align-items-center justify-content-between" style={{ width: '40rem' }}>
-                            <Card.Img   style={{maxWidth: '300px'}} src={foto}/>
-                            <Card.Body className="container m-1">
-                                <Card.Subtitle>Hotel</Card.Subtitle>
-                                <Card.Title className="my-2 fw-bold">{nome}</Card.Title>
-                                <Card.Text className="my-2 ">
+                        <Card className="m-3 p-0 d-flex flex-row" style={{maxWidth: '40rem' }}>
+                            <Card.Img className="float-start" src={foto} style={{maxWidth: '300px' }}/>
+                            <Card.Body className="col-auto container m-1">
+                                <Card.Subtitle>Hotel <Rating/> </Card.Subtitle>
+                                <Card.Title className="mb-1 fw-bold">{nome}</Card.Title>
+                                <Card.Text className="mb-1 ">
+                                    <MdRoom/>
                                     {localizacao + " . "}
-                                    <Card.Link className="my-2 text-decoration-none fw-bold">Mostrar no Mapa</Card.Link>
+                                    <Card.Link className="mb-1 text-decoration-none fw-bold">Mostrar no Mapa</Card.Link>
                                 </Card.Text>
                                 
-                                <Card.Text className="my-2">{descricao}</Card.Text>
+                                <Card.Text className="">{descricao}</Card.Text>
                                 <Card.Link className="btn btn-success  text-decoration-none" style={{width: '100%'}}>Ver Mais</Card.Link>
                             </Card.Body>
                         </Card>
