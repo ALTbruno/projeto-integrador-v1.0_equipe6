f402fe9167740725f20eaf4a59f3ffd12482161d
William Lima <wilima010@gmail.com>
2022-02-25 19:14:41 -0300

    crds atualizados

--- front/src/components/recomendationList/index.jsx
+++ front/src/components/recomendationList/index.jsx
@@ -1,9 +1,9 @@
 import { Button, Card, CardGroup, Container, Row, Col, Stack } from "react-bootstrap";
-import Rating from "../avaliationStars";
+import StarRating from "../avaliationStars";
 import hoteis from "./hoteis.json";
-import { MdRoom } from "react-icons/md";
+import { MdRoom, MdWifi, MdPool } from "react-icons/md";
 
-function RecomendationList () {
+const RecomendationList = () => {
 
     //Cartão 1: Criação de um cartão no qual cada produto será representado.
     //Cartão 2: Dentro do cartão inclua um bloco alinhado à esquerda que inclui uma imagem centralizada.
@@ -19,16 +19,17 @@ function RecomendationList () {
                         <Card className="m-3 p-0 d-flex flex-row" style={{maxWidth: '40rem' }}>
                             <Card.Img className="float-start" src={foto} style={{maxWidth: '300px' }}/>
                             <Card.Body className="col-auto container m-1">
-                                <Card.Subtitle>Hotel <Rating/> </Card.Subtitle>
+                                <Card.Subtitle className="mb-1 d-flex flex-row" style={{fontSize: '12px'}}>Hotel <StarRating/> </Card.Subtitle>
                                 <Card.Title className="mb-1 fw-bold">{nome}</Card.Title>
                                 <Card.Text className="mb-1 ">
-                                    <MdRoom/>
+                                    <MdRoom className="me-1"/>
                                     {localizacao + " . "}
-                                    <Card.Link className="mb-1 text-decoration-none fw-bold">Mostrar no Mapa</Card.Link>
+                                    <Card.Link className="text-decoration-none fw-bold">Mostrar no Mapa</Card.Link>
                                 </Card.Text>
-                                
-                                <Card.Text className="">{descricao}</Card.Text>
-                                <Card.Link className="btn btn-success  text-decoration-none" style={{width: '100%'}}>Ver Mais</Card.Link>
+                                <MdWifi className="me-1"/>
+                                <MdPool className="me-1"/>
+                                <Card.Text className="" style={{fontSize: '10px'}}>{descricao}</Card.Text>
+                                <Card.Link className="btn btn-success text-decoration-none" style={{width: '100%'}}>Ver Mais</Card.Link>
                             </Card.Body>
                         </Card>
                     )
