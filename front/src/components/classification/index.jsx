391c90d20aa9139d9c882bb919217a42999fc915
William Lima <wilima010@gmail.com>
2022-02-27 22:35:35 -0300

    a

--- front/src/components/classification/index.jsx
+++ front/src/components/classification/index.jsx
@@ -6,7 +6,7 @@ const Classification = () => {
    
     return (
         <>
-           <Stack className="d-inline-flex">
+           <Stack className="d-inline-flex justify-content-end" style={{width: '90px'}}>
                <div className="d-inline-flex flex-column justify-content-center">
                 <h5 className="text-center text-light rounded bg-dark w-50 mx-auto my-0">8</h5>
                 <p className="text-center fw-bold my-0" style={{fontSize: '10px'}}>Muito Bom</p>
