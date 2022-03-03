63e94aa34a7fcc26e23a7cf01e175810fdd8ca8c
Henrique Torres Rosa <zique.torres@gmail.com>
2022-03-03 11:51:32 -0300

    adição da pasta database com arquivos da sprint 1

---
+++ Database/primeira_sprint_PI.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE `projeto-integradorgp6`;
+
+CREATE TABLE categorias(
+id INT NOT NULL AUTO_INCREMENT,
+qualificacao VARCHAR(255),
+descricao VARCHAR(255),
+image_url VARCHAR(255),
+PRIMARY KEY(id)
+);
+
+
