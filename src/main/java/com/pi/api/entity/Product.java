4bc83a59fc2f58f6ff2198121826c1fcf135414d
Bruno Barbosa <brb.dev@outlook.com>
2022-03-15 00:30:20 -0400

    ajustes

--- src/main/java/com/pi/api/entity/Product.java
+++ src/main/java/com/pi/api/entity/Product.java
@@ -13,97 +13,107 @@ import java.util.Set;
 @Table(name = "products")
 public class Product {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @NotBlank
-    @Size(max = 255)
-    private String name;
+	@NotBlank
+	@Size(max = 255)
+	private String name;
 
-    @NotBlank
-    @Size(max = 255)
-    private String description;
+	@NotBlank
+	@Size(max = 255)
+	private String description;
 
-    @ManyToOne( cascade = CascadeType.ALL,fetch = FetchType.LAZY)
-    @JoinColumn(name = "id_category")
-    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-    private Category category;
+	@ManyToOne
+	@JoinColumn(name = "id_category")
+	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+	private Category category;
 
-    @ManyToOne( cascade = CascadeType.ALL,fetch = FetchType.LAZY)
-    @JoinColumn(name = "id_city")
-    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-    private City city;
+	@ManyToOne
+	@JoinColumn(name = "id_city")
+	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+	private City city;
 
-    @ManyToOne( cascade = CascadeType.ALL,fetch = FetchType.LAZY)
-    @JoinColumn(name = "id_image")
-    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-    private Image image;
+//	@OneToMany( cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+//	@JoinColumn(name = "id_image")
+//	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+//	private Image image;
 
-    @OneToMany(mappedBy ="products", cascade = CascadeType.ALL)
-    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-    private Set<Reservation> reservation = new HashSet<>();
+//	@OneToMany(mappedBy ="products", cascade = CascadeType.ALL)
+//	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+//	private Set<Reservation> reservation = new HashSet<>();
 
-    @OneToMany(mappedBy ="product", cascade = CascadeType.ALL)
-    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-    private Set<Characteristc> characteristcs = new HashSet<>();
-
-
-
-
-    public Product() {
-    }
-
-    public Product(String name, String description, Category category, City city, Image image) {
-        this.name = name;
-        this.description = description;
-        this.category = category;
-        this.city = city;
-        this.image = image;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	@ManyToMany
+	@JoinTable(name = "PRODUCT_CHARACTERISTIC",
+			joinColumns = @JoinColumn(name = "product_id"),
+			inverseJoinColumns = @JoinColumn(name = "characteristic_id"))
+	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+	private Set<Characteristic> characteristics;
 
 
-    public Category getCategory() {
-        return category;
-    }
 
-    public void setCategory(Category category) {
-        this.category = category;
-    }
 
-    public City getCity() {
-        return city;
-    }
+	public Product() {
+	}
 
-    public void setCity(City city) {
-        this.city = city;
-    }
+//	public Product(String name, String description, Category category, City city, Image image) {
+//		this.name = name;
+//		this.description = description;
+//		this.category = category;
+//		this.city = city;
+//		this.image = image;
+//	}
 
-    public Image getImage() {
-        return image;
-    }
+	public Product(String name, String description, Category category, City city) {
+		this.name = name;
+		this.description = description;
+		this.category = category;
+		this.city = city;
+	}
 
-    public void setImage(Image image) {
-        this.image = image;
-    }
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public City getCity() {
+		return city;
+	}
+
+	public void setCity(City city) {
+		this.city = city;
+	}
+
+//	public Image getImage() {
+//		return image;
+//	}
+//
+//	public void setImage(Image image) {
+//		this.image = image;
+//	}
 }
