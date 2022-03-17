73f65de8dcc41a1a79867cb7ae8c0d7c294cf231
Matheus da silva <ms25022003@gmail.com>
2022-03-17 16:11:31 -0300

    change routes

--- front/src/routes/index.jsx
+++ front/src/routes/index.jsx
@@ -3,6 +3,7 @@ import Footer from '../components/footer';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Header from '../components/header';
 import { Auth } from '../context/context';
+import Result from '../../src/components/result'
 import LoginPage from "../pages/Login";
 import RegisterPage from '../pages/Register';
 import PaginaProdutos from '../pages/paginaProdutos';
@@ -13,6 +14,7 @@ const RouteList = () => (
             <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/search/:city" element={<Result />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path='/register' element={<RegisterPage />} />
                 <Route path='/produto' element={<PaginaProdutos />} />
