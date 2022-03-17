4520eceb7b88b374066f6608abc1575c015385a5
Matheus da silva <ms25022003@gmail.com>
2022-03-17 12:04:39 -0300

    alteração baseurl axios

--- front/src/services/index.js
+++ front/src/services/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://pibd.herokuapp.com/'
+    baseURL: 'https://digital-booking.herokuapp.com/'
 });
 
 export default api;
