a283d5431368f65e81033d2a4bc35f4906cbfff8
Matheus da silva <ms25022003@gmail.com>
2022-03-18 20:18:13 -0300

    criação de rotas para pesquisa de hoteis/produtos

--- front/src/routes/index.jsx
+++ front/src/routes/index.jsx
@@ -1,9 +1,10 @@
-import Home from '../pages/Home';
-import Footer from '../components/footer';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import Header from '../components/header';
 import { Auth } from '../context/context';
-import Result from '../../src/components/result'
+import Footer from '../components/footer';
+import Header from '../components/header';
+import Home from '../pages/Home';
+import HoteisSearchByCity from '../pages/BuscaHoteisCity'
+import HoteisSearchByCategory from '../pages/BuscaHoteisCategoria';
 import LoginPage from "../pages/Login";
 import RegisterPage from '../pages/Register';
 import PaginaProdutos from '../pages/paginaProdutos';
@@ -14,7 +15,8 @@ const RouteList = () => (
             <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/search/:city" element={<Result />} />
+                <Route path="/search/:city" element={<HoteisSearchByCity />} />
+                <Route path="/category/:category" element={<HoteisSearchByCategory />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path='/register' element={<RegisterPage />} />
                 <Route path='/produto' element={<PaginaProdutos />} />
