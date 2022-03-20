a680222b9f4e2849c1c95a27dab6a276c514ad9b
Bruno Barbosa <brb.dev@outlook.com>
2022-03-20 16:45:54 -0400

    perfil de testes

---
+++ src/main/resources/test-database/data.sql
@@ -0,0 +1,119 @@
+INSERT INTO categories(title, description, image_url)
+	VALUES
+		('Hotéis', 'Comodidade e facilidade para conhecer a região', 'https://images2.imgbox.com/26/8b/L7yjUChE_o.jpg'),
+        ('Resorts', 'Infraestrutura completa em um só lugar', 'https://images2.imgbox.com/43/8c/8hIlAexo_o.jpg'),
+        ('Hostels', 'Troque experiências e conheça gente nova', 'https://images2.imgbox.com/42/48/loLbvmtJ_o.jpg'),
+        ('Apartamentos', 'Ideal para viagens em grupo', 'https://images2.imgbox.com/f8/03/BElptgf4_o.jpg'),
+        ('Pousadas', 'Conforto e aconchego', 'https://images2.imgbox.com/c6/25/Mx1fArhQ_o.jpg'),
+        ('Hotéis-fazenda', 'Experiência única com uma  variedade de atividades', 'https://images2.imgbox.com/31/37/nUQSAZ0Q_o.jpg'),
+        ('Chalés', 'Privativo e intimista', 'https://images2.imgbox.com/ec/41/iffWYEMm_o.jpg'),
+        ('Casas de campo', 'Paz e tranquilidade', 'https://images2.imgbox.com/04/25/zCnjvqQC_o.jpg'),
+        ('Motéis americanos', 'Ideal para quem está viajando de carro', 'https://images2.imgbox.com/3a/9e/pal8ewvO_o.jpg'),
+        ('Camas e Cafés (B&Bs)', 'Contato com os hábitos e cultura locais', 'https://images2.imgbox.com/f3/59/8ftUs7iS_o.jpg'),
+        ('Camping', 'Conexão com a natureza', 'https://images2.imgbox.com/d1/59/VF92U0Z7_o.jpg'),
+        ('Mini casas', 'Para aqueles que levam um estilo de vida minimalista', 'https://images2.imgbox.com/21/e7/2ZONLf89_o.jpg');
+
+INSERT INTO cities(name, country)
+	VALUES
+        ('São Paulo', 'Brasil'), ('Rio de Janeiro', 'Brasil'), ('Natal', 'Brasil'), ('Belém', 'Brasil'), ('Curitiba', 'Brasil'),
+        ('Belo Horizonte', 'Brasil'), ('Florianópolis', 'Brasil'), ('Brasília', 'Brasil'), ('Porto Alegre', 'Brasil'), ('Salvador', 'Brasil'),
+		('Belém', 'Brasil'), ('Fortaleza', 'Brasil'), ('Fernando de Noronha', 'Brasil'), ('Manaus', 'Brasil'), ('Gramado', 'Brasil'),
+		('Paraty', 'Brasil'), ('Campos do Jordão', 'Brasil'), ('Bonito', 'Brasil'), ('Jericoacoara', 'Brasil'), ('Angra dos Reis', 'Brasil'),
+		('Maceió', 'Brasil'), ('Porto Seguro', 'Brasil'), ('Ouro Preto', 'Brasil'), ('Diamantina', 'Brasil'), ('Petrópolis', 'Brasil'),
+		('Blumenau', 'Brasil'), ('Olinda', 'Brasil'), ('Pomerode', 'Brasil'), ('Holambra', 'Brasil'), ('Morretes', 'Brasil'),
+		('Bento Gonçalves', 'Brasil'), ('Monte Verde', 'Brasil'), ('Joinville', 'Brasil'), ('João Pessoa', 'Brasil'),
+        ('Santarém', 'Brasil'), ('Palmas', 'Brasil'), ('Cidade do Panamá', 'Panamá'), ('Assunção', 'Paraguai'),
+        ('Bucareste', 'Romênia'), ('Oslo', 'Noruega'), ('Madri', 'Espanha'), ('Santiago', 'Chile'), ('Porto', 'Portugal'),
+        ('Buenos Aires', 'Argentina'), ('Nova Iorque', 'Estados Unidos'), ('Toronto', 'Canadá'), ('Londres', 'Inglaterra'),
+        ('Paris', 'França'), ('Sydney', 'Austrália'), ('Tóquio', 'Japão'), ('Berlim', 'Alemanha'), ('Barcelona', 'Espanha');
+
+INSERT INTO characteristics(name, icon)
+	VALUES
+		('Wi-Fi', 'https://images2.imgbox.com/5f/56/D8z4Arzi_o.png'),
+		('Piscina', 'https://images2.imgbox.com/dd/82/WrPPtwSW_o.png'),
+		('Pet Friendly', 'https://images2.imgbox.com/58/a0/u0Hmag7Q_o.png'),
+		('TV', 'https://images2.imgbox.com/62/0b/7pQMfTbf_o.png'),
+		('Proibido Fumar', 'https://images2.imgbox.com/6a/83/DEYQqNqa_o.png'),
+		('Garagem', 'https://images2.imgbox.com/1e/2c/jUp7vCY3_o.png');
+
+INSERT INTO images(title, url)
+	VALUES
+		('A_IMG_01', 'https://images2.imgbox.com/34/fe/mm3dQ8MO_o.png'),
+        ('A_IMG_02', 'https://images2.imgbox.com/a0/ad/YYJtDZRA_o.png'),
+        ('A_IMG_03', 'https://images2.imgbox.com/73/8d/JYJUZfyN_o.png'),
+        ('A_IMG_04', 'https://images2.imgbox.com/e3/ab/eedfVWHq_o.png'),
+        ('A_IMG_05', 'https://images2.imgbox.com/fe/ed/H629bupw_o.png'),
+        ('A_IMG_06', 'https://images2.imgbox.com/96/d4/1SEXFjsg_o.png'),
+        ('B_IMG_01', 'https://images2.imgbox.com/34/f6/ycDc1X9C_o.png'),
+        ('B_IMG_02', 'https://images2.imgbox.com/b9/fe/IFixR8pw_o.png'),
+        ('B_IMG_03', 'https://images2.imgbox.com/37/b4/QiW7TNlq_o.png'),
+        ('B_IMG_04', 'https://images2.imgbox.com/7c/43/WQimeubW_o.png'),
+        ('B_IMG_05', 'https://images2.imgbox.com/91/6d/NpVvbPHf_o.png'),
+        ('B_IMG_06', 'https://images2.imgbox.com/40/db/Q1u8sIg6_o.png'),
+        ('C_IMG_01', 'https://images2.imgbox.com/bc/ae/UXy4Jtxc_o.png'),
+        ('C_IMG_02', 'https://images2.imgbox.com/19/fe/FBMXWRAS_o.png'),
+        ('C_IMG_03', 'https://images2.imgbox.com/0a/53/mccrGG2K_o.png'),
+        ('C_IMG_04', 'https://images2.imgbox.com/65/77/qvC4qPwH_o.png'),
+        ('C_IMG_05', 'https://images2.imgbox.com/d0/86/J2dpv0Zd_o.png'),
+        ('C_IMG_06', 'https://images2.imgbox.com/03/1a/VuRUh3b9_o.png'),
+        ('D_IMG_01', 'https://images2.imgbox.com/ea/de/plF3NbBx_o.png'),
+        ('D_IMG_02', 'https://images2.imgbox.com/8e/d8/K1SmCE94_o.png'),
+        ('D_IMG_03', 'https://images2.imgbox.com/ea/ec/4ojNC8Eh_o.png'),
+        ('D_IMG_04', 'https://images2.imgbox.com/4c/86/TAUos6A6_o.png'),
+        ('D_IMG_05', 'https://images2.imgbox.com/32/ef/YqvKlXVl_o.png'),
+        ('D_IMG_06', 'https://images2.imgbox.com/18/fb/cJdTzJtn_o.png'),
+        ('E_IMG_01', 'https://images2.imgbox.com/ec/84/epROpQwZ_o.png'),
+        ('E_IMG_02', 'https://images2.imgbox.com/27/78/Kv7wsX53_o.png'),
+        ('E_IMG_03', 'https://images2.imgbox.com/64/77/QUEpMBSV_o.png'),
+        ('E_IMG_04', 'https://images2.imgbox.com/a7/6d/0ivIzp7h_o.png'),
+        ('E_IMG_05', 'https://images2.imgbox.com/fb/c2/hY4CnH9v_o.png'),
+        ('E_IMG_06', 'https://images2.imgbox.com/c6/37/HXnHodc0_o.png'),
+        ('F_IMG_01', 'https://images2.imgbox.com/79/55/BXw0LDMl_o.png'),
+        ('F_IMG_02', 'https://images2.imgbox.com/b0/65/ApowEQTk_o.png'),
+        ('F_IMG_03', 'https://images2.imgbox.com/18/7c/euZL9i97_o.png'),
+        ('F_IMG_04', 'https://images2.imgbox.com/a8/2e/W9evPSjZ_o.png'),
+        ('F_IMG_05', 'https://images2.imgbox.com/f5/73/nAnkTxR8_o.png'),
+        ('F_IMG_06', 'https://images2.imgbox.com/20/cf/PS8MdkGZ_o.png'),
+        ('G_IMG_01', 'https://images2.imgbox.com/9b/a2/E5dG5c7C_o.png'),
+        ('G_IMG_02', 'https://images2.imgbox.com/c9/3e/MmcodPA5_o.png'),
+        ('G_IMG_03', 'https://images2.imgbox.com/f4/08/LoV7HpR5_o.png'),
+        ('G_IMG_04', 'https://images2.imgbox.com/8d/e1/CDCxSXra_o.png'),
+        ('G_IMG_05', 'https://images2.imgbox.com/1f/eb/k0ieWCbd_o.png'),
+        ('G_IMG_06', 'https://images2.imgbox.com/93/23/DE7KPk6T_o.png'),
+        ('Imagem 1', 'https://exp.cdn-hotels.com/hotels/10000000/9600000/9591000/9590960/3a2a687c_z.jpg'),
+        ('Imagem 2', 'https://exp.cdn-hotels.com/hotels/10000000/9600000/9591000/9590960/e98a712d_z.jpg'),
+        ('Imagem 3', 'https://exp.cdn-hotels.com/hotels/10000000/9600000/9591000/9590960/0f8e98d8_z.jpg'),
+        ('Imagem 4', 'https://exp.cdn-hotels.com/hotels/10000000/9600000/9591000/9590960/abaef732_z.jpg'),
+        ('Imagem 5', 'https://exp.cdn-hotels.com/hotels/10000000/9600000/9591000/9590960/6cadf98f_z.jpg');
+
+INSERT INTO products(name, description, id_category, id_city)
+	VALUES
+		('Produto A', 'Descrição do Produto A', 1, 1),
+		('Produto B', 'Descrição do Produto B', 1, 5),
+		('Produto C', 'Descrição do Produto C', 2, 1),
+		('Produto D', 'Descrição do Produto D', 3, 8),
+		('Produto E', 'Descrição do Produto E', 1, 9),
+		('Produto F', 'Descrição do Produto F', 5, 15),
+		('Produto G', 'Descrição do Produto G', 8, 23);
+
+
+INSERT INTO product_characteristic
+	VALUES
+		(1, 1), (1, 2), (1, 3), (1, 4),
+		(2, 1), (2, 2), (2, 4),
+		(3, 1), (3, 2), (3, 3), (3, 4),
+		(4, 1),
+		(5, 1), (5, 4),
+		(6, 1), (6, 3), (6, 5),
+		(7, 2), (7, 3), (7, 4), (7, 6);
+
+INSERT INTO products_images
+	VALUES
+		(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6),
+		(2, 7), (2, 8), (2, 9), (2, 10), (2, 11), (2, 12),
+		(3, 13), (3, 14), (3, 15), (3, 16), (3, 17), (3, 18),
+		(4, 19), (4, 20), (4, 21), (4, 22), (4, 23), (4, 24),
+		(5, 25), (5, 26), (5, 27), (5, 28), (5, 29), (5, 30),
+		(6, 31), (6, 32), (6, 33), (6, 34), (6, 35), (6, 36),
+		(7, 37), (7, 38), (7, 39), (7, 40), (7, 41), (7, 42);
+
