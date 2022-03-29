f20b4f9e27cd8cdfa97dd1a64ba0e0d5ff5e81f4
david115x0 <davidsilvafs29@gmail.com>
2022-03-29 10:17:09 -0300

    add id

--- src/main/java/com/pi/api/entity/Product.java
+++ src/main/java/com/pi/api/entity/Product.java
@@ -70,6 +70,14 @@ public class Product {
 		this.characteristics = characteristics;
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
 	}
