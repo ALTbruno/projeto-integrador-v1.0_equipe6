59de6ebdf90531e01557c3e7e15b198b94270ef3
Matheus Silva <ms25022003@gmail.com>
2022-04-11 10:46:42 -0300

    resolução bug map

--- front/src/pages/paginaProdutos/index.jsx
+++ front/src/pages/paginaProdutos/index.jsx
@@ -17,7 +17,7 @@ const PaginaProdutos = () => {
     const [largeWidth, setLargeWidth] = useState(false);
     const navegar = useNavigate()
     const { id } = useParams();
-    const [produtos, setProdutos] = useState({  
+    const [produtos, setProdutos] = useState({
         "id": null,
         "name": "",
         "description": "",
@@ -66,48 +66,51 @@ const PaginaProdutos = () => {
                 "name": "",
                 "icon": ""
             }
-        ]
+        ],
+        "latitude": 0,
+        "longitude": 0,
     });
 
 
     useEffect(() => {
-        api.get(`/products/${id}`).then(response => {setProdutos(response.data);
+        api.get(`/products/${id}`).then(response => {
+            setProdutos(response.data);
         })
     }, [id]);
-  
+
     useEffect(() => {
-      window.innerWidth > 559 ? setLargeWidth(true) : setLargeWidth(false);
-      const handleResize = () => {
-        if (window.innerWidth > 559) {
-          setLargeWidth(true);
-        } else {
-          setLargeWidth(false);
-        }
-      };
-  
-      window.addEventListener('resize', handleResize);
-  
-      return () => window.removeEventListener('resize', handleResize);
+        window.innerWidth > 559 ? setLargeWidth(true) : setLargeWidth(false);
+        const handleResize = () => {
+            if (window.innerWidth > 559) {
+                setLargeWidth(true);
+            } else {
+                setLargeWidth(false);
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => window.removeEventListener('resize', handleResize);
     }, []);
-  
-    const days = [ 'Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado' ];
+
+    const days = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado'];
     const months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
     const locale = {
-      localize: {
-        day: (n) => days[n],
-        month: (n) => months[n],
-      },
-      formatLong: {
-        date: () => 'mm/dd/yyyy',
-      },
+        localize: {
+            day: (n) => days[n],
+            month: (n) => months[n],
+        },
+        formatLong: {
+            date: () => 'mm/dd/yyyy',
+        },
     };
-  
+
     const setValuesDate = (dates) => {
-      const [start, end] = dates;
-      setStartDate(start);
-      setEndDate(end);
+        const [start, end] = dates;
+        setStartDate(start);
+        setEndDate(end);
     };
-    
+
     return (
         <>
             {/* Bloco de Titulo */}
@@ -131,8 +134,8 @@ const PaginaProdutos = () => {
 
             {/* Bloco de Imagem */}
             <div className="p-2 border d-flex align-items-center justify-content-center mw-100">
-            
-    
+
+
                 <div className="p-1 w-50" >
                     <img key={produtos.images[0].id} className="img-fluid rounded-3" src={produtos.images[0].url} alt="" srcSet="" />
                 </div>
@@ -149,7 +152,7 @@ const PaginaProdutos = () => {
                 <div className="w-25" >
                     <div className="p-1 w-100">
                         <img key={produtos.images[3].id} className="img-fluid rounded-3" src={produtos.images[3].url} alt="" srcSet="" />
-                     </div>
+                    </div>
                     <div className="p-1 w-100 position-relative">
                         <ProdutosModal />
                         <img key={produtos.images[4].id} className="img-fluid rounded-3" src={produtos.images[4].url} alt="" srcSet="" />
@@ -174,57 +177,57 @@ const PaginaProdutos = () => {
                     <h2 className="ms-3" style={{ backgroundColor: "#bfbfbf" }}>Caracteristicas</h2>
                 </div>
                 <div className="my-3 d-flex justify-content-around">
-                {produtos.characteristics.map(characteristic => (
-                    <div className="p-1 d-flex flex-row" key={characteristic.id}>
-                        <img className="me-1" src={characteristic.icon} alt={characteristic.name} style={{width: '20px'}}/>
-                        <p className="ms-1">{characteristic.name}</p>
-                    </div>
-                ))}
+                    {produtos.characteristics.map(characteristic => (
+                        <div className="p-1 d-flex flex-row" key={characteristic.id}>
+                            <img className="me-1" src={characteristic.icon} alt={characteristic.name} style={{ width: '20px' }} />
+                            <p className="ms-1">{characteristic.name}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
 
             {/* Calendario */}
             <section className="container-reserva">
-        <h1>Datas disponiveis</h1>
-        <div className="container-calendar">
-          <DatePicker
-            inline
-            id="calendar"
-            locale={locale}
-            formatWeekDay={(locale) => locale[0]}
-            placeholder={true}
-            selected={startDate}
-            onChange={setValuesDate}
-            setOpen={true}
-            shouldCloseOnSelect={false}
-            startDate={startDate}
-            endDate={endDate}
-            minDate={new Date() - 2}
-            dateFormat="dd/MM/yyyy"
-            selectsRange={true}
-            monthsShown={largeWidth ? 2 : 1}
-          />
-          <div className="content-calendar">
-            <h3>Adicione as datas da sua viagem para obter preço exatos</h3>
-            <Link className="btn border-3 w-100 p-2 text-light fw-bold" style={{ backgroundColor: '#1DBEB4', border: '#1DBEB4' }} to={`/produto/${id}/reserva`}>Iniciar reserva</Link>
-          </div>
-        </div>
-      </section>
+                <h1>Datas disponiveis</h1>
+                <div className="container-calendar">
+                    <DatePicker
+                        inline
+                        id="calendar"
+                        locale={locale}
+                        formatWeekDay={(locale) => locale[0]}
+                        placeholder={true}
+                        selected={startDate}
+                        onChange={setValuesDate}
+                        setOpen={true}
+                        shouldCloseOnSelect={false}
+                        startDate={startDate}
+                        endDate={endDate}
+                        minDate={new Date() - 2}
+                        dateFormat="dd/MM/yyyy"
+                        selectsRange={true}
+                        monthsShown={largeWidth ? 2 : 1}
+                    />
+                    <div className="content-calendar">
+                        <h3>Adicione as datas da sua viagem para obter preço exatos</h3>
+                        <Link className="btn border-3 w-100 p-2 text-light fw-bold" style={{ backgroundColor: '#1DBEB4', border: '#1DBEB4' }} to={`/produto/${id}/reserva`}>Iniciar reserva</Link>
+                    </div>
+                </div>
+            </section>
 
             {/* Mapa */}
-            
+
             <div className="mb-2 p-3">
                 <div className=" my-3 p-1 rounded-3" style={{ backgroundColor: "#bfbfbf" }}>
                     <h2 className="ms-3 " style={{ backgroundColor: "#bfbfbf" }}>Onde você vai estar</h2>
                 </div>
                 <div className="d-flex flex-column align-items-center">
-                    <Map/>
+                    <Map product={produtos} />
                 </div>
             </div>
 
             {/* Bloco Politicas do Produto */}
             <div className="p-3">
-                <div className="p-1 rounded-3" style={{ backgroundColor: "#bfbfbf" }}>    
+                <div className="p-1 rounded-3" style={{ backgroundColor: "#bfbfbf" }}>
                     <h2 className="ms-3 " style={{ backgroundColor: "#bfbfbf" }}>Bloco Politicas do Produto</h2>
                 </div>
                 <div className="p-1">
