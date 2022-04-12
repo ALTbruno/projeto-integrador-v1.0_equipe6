8de7aaef753f3cf955997736b9b0e3b301458a96
Bruno Barbosa <brb.dev@outlook.com>
2022-04-12 19:02:00 -0400

    ajuste cors

--- src/main/java/com/pi/api/controller/CategoryController.java
+++ src/main/java/com/pi/api/controller/CategoryController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-@CrossOrigin(origins = "*", allowedHeaders = "")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
--- src/main/java/com/pi/api/controller/ProductController.java
+++ src/main/java/com/pi/api/controller/ProductController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-@CrossOrigin(origins = "*", allowedHeaders = "")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
