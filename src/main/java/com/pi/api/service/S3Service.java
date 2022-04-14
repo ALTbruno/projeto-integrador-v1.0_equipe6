75684eaf00e1ec18ed97f6b665a26cad654d7c86
Bruno Barbosa <brb.dev@outlook.com>
2022-04-14 15:47:03 -0400

    aws s3 config

---
+++ src/main/java/com/pi/api/service/S3Service.java
@@ -0,0 +1,40 @@
+package com.pi.api.service;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+@Service
+public class S3Service {
+
+	@Autowired
+	private AmazonS3 s3client;
+
+	@Value("${bucketName}")
+	private String bucketName;
+
+	public File convertMultiPartToFile(MultipartFile file) throws IOException {
+		File convertedFile = new File(file.getOriginalFilename());
+		FileOutputStream fos = new FileOutputStream(convertedFile);
+		fos.write(file.getBytes());
+		fos.close();
+		return convertedFile;
+	}
+
+	public String uploadFileTos3bucket(String directory, String fileName, File file) {
+		try {
+			s3client.putObject(new PutObjectRequest(bucketName, directory + fileName, file));
+		}catch(AmazonServiceException e) {
+			return "uploadFileTos3bucket().Uploading failed :" + e.getMessage();
+		}
+		return "Uploading Successfull -> ";
+	}
+}
