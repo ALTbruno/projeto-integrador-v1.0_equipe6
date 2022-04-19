76d7e46a54b33f792c0ce098ee95822c41df25c1
Bruno Barbosa <brb.dev@outlook.com>
2022-04-19 18:50:14 +0000

    agora vai?

--- src/main/java/com/pi/api/service/CharacteristicService.java
+++ src/main/java/com/pi/api/service/CharacteristicService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.io.IOException;
 
 @Service
